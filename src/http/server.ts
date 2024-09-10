fa8267202c598cf417ae61a7447acd9b2a1053c9
black-adm <mattheuscontato17@gmail.com>
2024-09-09 22:58:43 -0300

    feat: create goal service and route

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,10 +1,41 @@
 import fastify from "fastify";
+import z from "zod";
 
-const app = fastify();
+import {
+  type ZodTypeProvider,
+  serializerCompiler,
+  validatorCompiler,
+} from "fastify-type-provider-zod";
+import { createGoal } from "../services/createGoal";
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.post(
+  "/create-goal",
+  {
+    schema: {
+      body: z.object({
+        title: z.string(),
+        desiredWeeklyFrequency: z.number().int().min(1).max(7),
+      }),
+    },
+  },
+  async (request) => {
+    const { title, desiredWeeklyFrequency } = request.body;
+
+    await createGoal({
+      title,
+      desiredWeeklyFrequency,
+    });
+  }
+);
 
 app
   .listen({
-    port: 3333,
+    port: 4000,
   })
   .then(() => {
     console.log("🚀 HTTP server running ...");
---
+++ src/services/createGoal.ts
@@ -0,0 +1,22 @@
+import { db } from "../db/connection";
+import { goals } from "../db/schema";
+import type { CreateGoalRequest } from "../types/goal";
+
+export async function createGoal({
+  title,
+  desiredWeeklyFrequency,
+}: CreateGoalRequest) {
+  const result = await db
+    .insert(goals)
+    .values({
+      title,
+      desiredWeeklyFrequency,
+    })
+    .returning();
+
+  const goal = result[0];
+
+  return {
+    goal,
+  };
+}
